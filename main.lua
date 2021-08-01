626472b7fc331b8bb21b3a67e25b422f5d104cc4
wally <38594850+wally-rblx@users.noreply.github.com>
2021-08-01 08:09:29 -0500

    Added proposed keybinds system.
    Added proposed keybinds system, did some internal changes.

--- main.lua
+++ main.lua
@@ -1,41 +1,50 @@
--- updated 5/12/21
--- should choke less
+-- 8/1/21
+-- ! KRNL is currently broken, wait for them to fix it
 
--- updated 5/16/21
--- should ignore invisible notes
--- added hit chances and a toggle
--- hit chances are a bit rough but should work good enough
+-- + Added 'Manual' mode which allows you to force the notes to hiw a specific type by holding down a keybind.
+-- * Switched fastWait and fastSpawn to Roblox's task libraries
+-- * Attempted to fix 'invalid key to next' errors
 
--- only tested on Synapse X
--- moved ui to github & removed the kick
+-- 5/12/21
+-- Attempted to fix the autoplayer missing as much.
 
--- for "free exploit" developers
--- you need the following functions
--- loadstring, HttpGet, getgc, getloadedmodules, getconnections, and set_thread_identity or whatever you call it
+-- 5/16/21
+-- Attempt to fix invisible notes.
+-- Added hit chances & an autoplayer toggle
+-- ! Hit chances are a bit rough but should work.
+
+-- I have only tested on Synapse X 
+-- Script-Ware v2 should work fine
+-- KRNL should work fine (8/1/21: KRNL is currently having issues, wait for a patch)
+
+-- Needed functions: setthreadcontext, getconnections, getgc, getloaodedmodules 
+-- That should be it!
+
+-- You may find some contact information on the GitHub repo.
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/uwuware-ui/main/main.lua"))()
 
 local framework, scrollHandler
 while true do
-	for _, obj in next, getgc(true) do
-		if type(obj) == 'table' and rawget(obj, 'GameUI') then
-			framework = obj;
-			break
-		end	
-	end
+    for _, obj in next, getgc(true) do
+        if type(obj) == 'table' and rawget(obj, 'GameUI') then
+            framework = obj;
+            break
+        end 
+    end
 
-	for _, module in next, getloadedmodules() do
-		if module.Name == 'ScrollHandler' then
-			scrollHandler = module;
-			break;
-		end
-	end
+    for _, module in next, getloadedmodules() do
+        if module.Name == 'ScrollHandler' then
+            scrollHandler = module;
+            break;
+        end
+    end
 
-	if (type(framework) == 'table') and (typeof(scrollHandler) == 'Instance') then
-		break
-	end
+    if (type(framework) == 'table') and (typeof(scrollHandler) == 'Instance') then
+        break
+    end
 
-	wait(1)
+    wait(1)
 end
 
 local runService = game:GetService('RunService')
@@ -47,62 +56,71 @@ local task = task or getrenv().task;
 local fastWait, fastSpawn = task.wait, task.spawn;
 
 local fireSignal, rollChance do
-	-- updated for script-ware or whatever
-	-- attempted to update for krnl 
-	local set_identity = (type(syn) == 'table' and syn.set_thread_identity) or setidentity or setthreadcontext
-	function fireSignal(target, signal, ...)
-		-- getconnections with InputBegan / InputEnded does not work without setting Synapse to the game's context level
-		set_identity(2) 
-		for _, signal in next, getconnections(signal) do
-			if type(signal.Function) == 'function' and islclosure(signal.Function) then
-				local scr = rawget(getfenv(signal.Function), 'script')
-				if scr == target then
-					pcall(signal.Function, ...)
-				end
-			end
-		end
-		set_identity(7)
-	end
+    -- updated for script-ware or whatever
+    -- attempted to update for krnl 
+    local set_identity = (type(syn) == 'table' and syn.set_thread_identity) or setidentity or setthreadcontext
+    function fireSignal(target, signal, ...)    
+        -- getconnections with InputBegan / InputEnded does not work without setting Synapse to the game's context level
+        set_identity(2) 
+        for _, signal in next, getconnections(signal) do
+            if type(signal.Function) == 'function' and islclosure(signal.Function) then
+                local scr = rawget(getfenv(signal.Function), 'script')
+                if scr == target then
+                    pcall(signal.Function, ...)
+                end
+            end
+        end
+        set_identity(7)
+    end
 
-	-- uses a weighted random system
-	-- its a bit scuffed rn but it works good enough
+    -- uses a weighted random system
+    -- its a bit scuffed rn but it works good enough
 
-	function rollChance()
-		local chances = {
-			{ type = 'Sick', value = library.flags.sickChance },
-			{ type = 'Good', value = library.flags.goodChance },
-			{ type = 'Ok', value = library.flags.okChance },
-			{ type = 'Bad', value = library.flags.badChance },
-		}
-		
-		table.sort(chances, function(a, b) 
-			return a.value > b.value 
-		end)
+    function rollChance()
+        if (library.flags.autoPlayerMode == 'Manual') then
+            if (library.flags.sickHeld) then return 'Sick' end
+            if (library.flags.goodHeld) then return 'Good' end
+            if (library.flags.okayHeld) then return 'Ok' end
+            if (library.flags.missHeld) then return 'Bad' end
 
-		local sum = 0;
-		for i = 1, #chances do
-			sum += chances[i].value
-		end
+            return 'Bad' -- incase if it cant find one
+        end
 
-		if sum == 0 then
-			-- forgot to change this before?
-			-- fixed 6/5/21
-			return chances[random:NextInteger(1, 4)].type 
-		end
+        local chances = {
+            { type = 'Sick', value = library.flags.sickChance },
+            { type = 'Good', value = library.flags.goodChance },
+            { type = 'Ok', value = library.flags.okChance },
+            { type = 'Bad', value = library.flags.badChance },
+        }
+        
+        table.sort(chances, function(a, b) 
+            return a.value > b.value 
+        end)
 
-		local initialWeight = random:NextInteger(0, sum)
-		local weight = 0;
+        local sum = 0;
+        for i = 1, #chances do
+            sum += chances[i].value
+        end
 
-		for i = 1, #chances do
-			weight = weight + chances[i].value
+        if sum == 0 then
+            -- forgot to change this before?
+            -- fixed 6/5/21
+            return chances[random:NextInteger(1, 4)].type 
+        end
 
-			if weight > initialWeight then
-				return chances[i].type
-			end
-		end
+        local initialWeight = random:NextInteger(0, sum)
+        local weight = 0;
 
-		return 'Sick' -- just incase it fails?
-	end
+        for i = 1, #chances do
+            weight = weight + chances[i].value
+
+            if weight > initialWeight then
+                return chances[i].type
+            end
+        end
+
+        return 'Sick' -- just incase it fails?
+    end
 end
 
 local map = { [0] = 'Left', [1] = 'Down', [2] = 'Up', [3] = 'Right', }
@@ -111,76 +129,96 @@ local keys = { Up = Enum.KeyCode.Up; Down = Enum.KeyCode.Down; Left = Enum.KeyCo
 -- they are "weird" because they are in the middle of their Upper & Lower ranges 
 -- should hopefully make them more precise!
 local chanceValues = {
-	Sick = 96,
-	Good = 92,
-	Ok = 87,
-	Bad = 77,
+    Sick = 96,
+    Good = 92,
+    Ok = 87,
+    Bad = 77,
 }
 
 local marked = {}
 local hitChances = {}
 
 if shared._id then
-	pcall(runService.UnbindFromRenderStep, runService, shared._id)
+    pcall(runService.UnbindFromRenderStep, runService, shared._id)
 end
 
 shared._id = game:GetService('HttpService'):GenerateGUID(false)
 runService:BindToRenderStep(shared._id, 1, function()
-	if (not library.flags.autoPlayer) then return end
+    if (not library.flags.autoPlayer) then return end
 
-	for i, arrow in next, framework.UI.ActiveSections do
-		if (arrow.Side == framework.UI.CurrentSide) and (not marked[arrow]) then 
-			local indice = (arrow.Data.Position % 4) -- mod 4 because 5%4 -> 0, 6%4 = 1, etc
-			local position = map[indice]
-			
-			if (position) then
-				local currentTime = framework.SongPlayer.CurrentlyPlaying.TimePosition
-				local distance = (1 - math.abs(arrow.Data.Time - currentTime)) * 100
+    -- local list = framework.UI.ActiveSections
+    local arrows = {}
+    for _, obj in next, framework.UI.ActiveSections do
+        arrows[#arrows + 1] = obj;
+    end
 
-				if (arrow.Data.Time == 0) then
-				--	print('invisible', tableToString(arrow.Data), i, distance)
-					continue
-				end
+    for idx = 1, #arrows do
+        local arrow = list[idx]
+        if type(arrow) ~= 'table' then 
+            continue
+        end
 
-				local hitChance = hitChances[arrow] or rollChance()
-				hitChances[arrow] = hitChance
+        if (arrow.Side == framework.UI.CurrentSide) and (not marked[arrow]) then
+            -- 0 1 2 3
+            -- 4 5 6 7 (number % 4)
 
-				-- if (not chanceValues[hitChance]) then warn('invalid chance', hitChance) end
-				if distance >= chanceValues[hitChance] then
-					marked[arrow] = true;
-					fireSignal(scrollHandler, userInputService.InputBegan, { KeyCode = keys[position], UserInputType = Enum.UserInputType.Keyboard }, false)
+            local indice = (arrow.Data.Position % 4)
+            local position = map[indice]
+            
+            if (position) then
+                local currentTime = framework.SongPlayer.CurrentlyPlaying.TimePosition
+                local distance = (1 - math.abs(arrow.Data.Time - currentTime)) * 100
 
-					-- wait depending on the arrows length so the animation can play
-					if arrow.Data.Length > 0 then
-						fastWait(arrow.Data.Length)
-					else
-						fastWait(0.075) -- 0.1 seems to make it miss more, this should be fine enough?
-					end
+                if (arrow.Data.Time == 0) then
+                    continue
+                end
 
-					fireSignal(scrollHandler, userInputService.InputEnded, { KeyCode = keys[position], UserInputType = Enum.UserInputType.Keyboard }, false)
-					marked[arrow] = false;
-				end
-			end
-		end
-	end
+                local hitChance = hitChances[arrow] or rollChance()
+                hitChances[arrow] = hitChance
+
+                -- if (not chanceValues[hitChance]) then warn('invalid chance', hitChance) end
+                if distance >= chanceValues[hitChance] then
+                    marked[arrow] = true;
+                    fireSignal(scrollHandler, userInputService.InputBegan, { KeyCode = keys[position], UserInputType = Enum.UserInputType.Keyboard }, false)
+
+                    -- wait depending on the arrows length so the animation can play
+                    if arrow.Data.Length > 0 then
+                        fastWait(arrow.Data.Length)
+                    else
+                        fastWait(0.075) -- 0.1 seems to make it miss more, this should be fine enough?
+                    end
+
+                    fireSignal(scrollHandler, userInputService.InputEnded, { KeyCode = keys[position], UserInputType = Enum.UserInputType.Keyboard }, false)
+                    marked[arrow] = false;
+                end
+            end
+        end
+    end
 end)
 
 local window = library:CreateWindow('Funky Friday') do
-	local folder = window:AddFolder('Main') do
-		folder:AddToggle({ text = 'Autoplayer', flag = 'autoPlayer' })
+    local folder = window:AddFolder('Main') do
+        folder:AddToggle({ text = 'Autoplayer', flag = 'autoPlayer' })
+        folder:AddList({ text = 'Autoplayer mode', flag = 'autoPlayerMode', values = { 'Chances', 'Manual' } })
 
-		folder:AddSlider({ text = 'Sick %', flag = 'sickChance', min = 0, max = 100, value = 100 })
-		folder:AddSlider({ text = 'Good %', flag = 'goodChance', min = 0, max = 100, value = 0 })
-		folder:AddSlider({ text = 'Ok %', flag = 'okChance', min = 0, max = 100, value = 0 })
-		folder:AddSlider({ text = 'Bad %', flag = 'badChance', min = 0, max = 100, value = 0 })
-	end
+        folder:AddSlider({ text = 'Sick %', flag = 'sickChance', min = 0, max = 100, value = 100 })
+        folder:AddSlider({ text = 'Good %', flag = 'goodChance', min = 0, max = 100, value = 0 })
+        folder:AddSlider({ text = 'Ok %', flag = 'okChance', min = 0, max = 100, value = 0 })
+        folder:AddSlider({ text = 'Bad %', flag = 'badChance', min = 0, max = 100, value = 0 })
+    end
 
-	local folder = window:AddFolder('Credits') do
-		folder:AddLabel({ text = 'Credits' })
-		folder:AddLabel({ text = 'Jan - UI library' })
-		folder:AddLabel({ text = 'wally - Script' })
-	end
+    local folder = window:AddFolder('Keybinds') do
+        folder:AddBind({ text = 'Sick', flag = 'sickBind', key = 'One', callback = function(val) library.flags.sickHeld = (not val) end, })
+        folder:AddBind({ text = 'Good', flag = 'goodBind', key = 'Two', callback = function(val) library.flags.goodHeld = (not val) end, })
+        folder:AddBind({ text = 'Ok', flag = 'okBind', key = 'Three', callback = function(val) library.flags.okayHeld = (not val) end, })
+        folder:AddBind({ text = 'Bad', flag = 'badBind', key = 'Four', callback = function(val) library.flags.missHeld = (not val) end, })
+    end
+
+    local folder = window:AddFolder('Credits') do
+        folder:AddLabel({ text = 'Credits' })
+        folder:AddLabel({ text = 'Jan - UI library' })
+        folder:AddLabel({ text = 'wally - Script' })
+    end
 end
-
 
 library:Init()
